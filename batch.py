746c6aee4c5a20c7f47ebc3555b09dd12cc322d5
Kevin Tam <tam.kevin@hotmail.com>
2018-11-10 13:01:00 -0700

    Add ability to add custom name and file type

--- batch.py
+++ batch.py
@@ -8,15 +8,19 @@ print("Hello this program is designed to perform a batch rename of your files.")
 
 print(os.getcwd() + " is your current working directory\n")
 
+newFileName = raw_input("What would you like to name the file?\n")
+fileFormat = raw_input("What is the file type?\n")
+
 pwd = raw_input("Where are your files located? eg:(C:\ )\n")
+os.chdir(os.getcwd() + pwd) #Change to this directory
+
 #TODO add try and exceptions
-print(os.chdir(os.getcwd() + pwd)) #Change to this directory
 
 #Loop through the file names
 x = 0
 for fileNames in os.listdir(os.getcwd()):	
 	y = str(x)
-	os.rename(fileNames,"hello" + y + ".txt" )
+	os.rename(fileNames,newFileName + "S0E" + y + fileFormat )
 	x +=1
 
 print(os.listdir(os.getcwd())) #List all the files in the current working directory
