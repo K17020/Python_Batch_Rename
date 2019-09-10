0cd10f34c984abae6f24e8351d9aa27f33ae370a
Kevin Tam <tam.kevin@hotmail.com>
2019-09-10 04:27:29 -0700

    Simplified Code

--- batch.py
+++ batch.py
@@ -1,26 +1,13 @@
 # Batch rename 
 # Kevin Tam - version 0.1
 
-import os, shutil
-#TODO add it so you can exit the program
-#Add loop
-print("Hello this program is designed to perform a batch rename of your files.")
+import os
 
-print(os.getcwd() + " is your current working directory\n")
+file_number = 1
 
-newFileName = raw_input("What would you like to name the file?\n")
-fileFormat = raw_input("What is the file type?\n")
+os.chdir("name_of_directory") #current working directory
+file_name = os.getcwd()
 
-pwd = raw_input("Where are your files located? eg:(C:\ )\n")
-os.chdir(os.getcwd() + pwd) #Change to this directory
-
-#TODO add try and exceptions
-
-#Loop through the file names
-x = 0
-for fileNames in os.listdir(os.getcwd()):	
-	y = str(x)
-	os.rename(fileNames,newFileName + "S0E" + y + fileFormat )
-	x +=1
-
-print(os.listdir(os.getcwd())) #List all the files in the current working directory
+for files in os.listdir(str(file_name)):
+    renamed_file = os.rename(files, "The_name_of_the_file"+ str(file_number) + ".txt")
+    file_number += 1
